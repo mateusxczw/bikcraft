17a062027becc377a6b193810bf6ae0ca746780b
Mateus Cardozo <mateusczdev@gmail.com>
2024-07-11 20:42:54 -0300

    javascript update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,35 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Pergutas Frequentes
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
